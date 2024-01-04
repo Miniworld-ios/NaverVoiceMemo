3e849f47dfd617755fe40e23cb65766419b8e014
Min <jeong0983@naver.com>
2024-01-04 14:06:57 +0900

    Initial Commit

---
+++ NaverVoiceMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NaverVoiceMemo
+//
+//  Created by 심정민 on 2024/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NaverVoiceMemo/NaverVoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverVoiceMemoApp.swift
+//  NaverVoiceMemo
+//
+//  Created by 심정민 on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct NaverVoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
